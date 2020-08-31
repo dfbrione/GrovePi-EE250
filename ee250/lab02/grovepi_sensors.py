7a91a1d4d08c2a47653bbb3430a0005dc8d131b8
Daniel Briones <dfbrione@usc.edu>
2020-08-31 01:10:11 -0700

    GM_commented

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -3,7 +3,7 @@
 Daniel Briones
 Dfbrione@usc.edu
 
-GitHub Repository Link: https://github.com/dfbrione/GrovePi-EE250.git
+GitHub Repository Link: https://github.com/dfbrione/GrovePi-EE250/tree/lab02
 """
 
 """python3 interpreters in Ubuntu (and other linux distros) will look in a 
@@ -26,18 +26,18 @@ sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
 from grove_rgb_lcd import*
-
-def splash_screen():
+ 
+def splash_screen(): #Opening screen that displays the lab number and title
     setRGB(0,0,255) #Initialize the backlight as blue
     setText_norefresh("Lab02:\nGrovePi Sensors")
     time.sleep(2) #Pause for 2 seconds
 
-def not_in_range_screen(threshold_value, curr_ranger_output):
+def not_in_range_screen(threshold_value, curr_ranger_output): #Display screen stating current threshold and ultrasonic range output
     setRGB(0,255,0) #Set the backlight as green 
-    setText_norefresh(" " + str(threshold_value) + "cm" + "\n " + str(curr_ranger_output) + "cm")
+    setText_norefresh(" " + str(threshold_value) + "cm" + "\n " + str(curr_ranger_output) + "cm") 
     
-def in_range_screen(threshold_value, curr_ranger_output):
-    setRGB(255,0,0)
+def in_range_screen(threshold_value, curr_ranger_output): #Display screen stating object is in range, current threshold, and ranger value
+    setRGB(255,0,0) #Set the backlight as red
     setText_norefresh(str(threshold_value) + "cm OBJ PRES" + "\n " + str(curr_ranger_output) + "cm")
 
 """This if-statement checks if you are running this python file directly. That 
@@ -45,10 +45,12 @@ is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will
 be true"""
 if __name__ == '__main__':
     PORT = 4    # D4
-    pot = 0 #Analog port A0
-    grovepi.pinMode(pot, "INPUT")
+    pot = 0 #Analog port A0 (Potentiometer)
+    grovepi.pinMode(pot, "INPUT") #initialize the Pot
     splash_screen()
-    state = 0
+    state = 0 #variable used to check if screen needs to be cleared. 	
+    		  #Screen will need to be cleared if state has changed to prevent
+    		  #text from a previous state staying on the LCD(as we are not refreshing)														
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
@@ -57,19 +59,20 @@ if __name__ == '__main__':
         pot_value = grovepi.analogRead(pot)
         ranger_value = grovepi.ultrasonicRead(PORT)
         
-        if (ranger_value >=  pot_value):
-            if state!=1:
+        if (ranger_value >=  pot_value): 
+            
+            if state!=1: #Only clear screen if necessary
                 textCommand(0x01)
 
             not_in_range_screen(pot_value, ranger_value)
-            state = 1
+            state = 1 #Screen will not clear constantly if state hasn't changed
 
         else:
-            if state!= 2:
+            if state!= 2: #Only clear screen if necessary
                 textCommand(0x01)
                 
             in_range_screen(pot_value,ranger_value)
-            state=2
+            state=2 #Screen will not clear constantly if state hasn't changed
 
 
        # print(grovepi.ultrasonicRead(PORT))
